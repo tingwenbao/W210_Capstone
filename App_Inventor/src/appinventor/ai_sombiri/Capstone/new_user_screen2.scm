fbbcd92f83ed21deb1740b657986a89c16803bba
Sombiri Enwemeka <sombiri@enwemeka.com>
2018-03-24 12:42:35 -0700

    - Rearranged and renamed App inventor screens for clarity - Unpacked and added app files so individual file changes will be tracked - Added tinydb to each app screen for proper app globals, eg storing   server info, login credentials, etc.

---
+++ App_Inventor/src/appinventor/ai_sombiri/Capstone/new_user_screen2.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"167","Source":"Form","Properties":{"$Name":"new_user_screen2","$Type":"Form","$Version":"23","AppName":"Capstone","Title":"new_user_screen2","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"TinyDB1","$Type":"TinyDB","$Version":"1","Uuid":"-546656232"}]}}
+|#
---
+++ App_Inventor/src/appinventor/ai_sombiri/Capstone/pictureOCR.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"167","Source":"Form","Properties":{"$Name":"pictureOCR","$Type":"Form","$Version":"23","AppName":"Capstone","Title":"pictureOCR","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"TakePic","$Type":"Button","$Version":"6","Text":"Take Picture","Uuid":"-1244908337"},{"$Name":"post","$Type":"Button","$Version":"6","Text":"post","Uuid":"1902205218"},{"$Name":"Image1","$Type":"Image","$Version":"3","Uuid":"1906379807"},{"$Name":"Label1","$Type":"Label","$Version":"4","Text":"Text for Label1","Uuid":"-1504569336"},{"$Name":"Camera1","$Type":"Camera","$Version":"3","Uuid":"-2138643519"},{"$Name":"Web1","$Type":"Web","$Version":"4","Uuid":"-81602916"},{"$Name":"TaifunFile1","$Type":"TaifunFile","$Version":"8","Uuid":"2081768709"},{"$Name":"TinyDB1","$Type":"TinyDB","$Version":"1","Uuid":"-488721478"}]}}
+|#
