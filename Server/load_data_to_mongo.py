tingwenbao/W210_Capstone

commit 8152c276c5896b79f2056799032b4be98f458a05

Database server explicitly handles object IDs
- Removed objectID generation from db_object class
    - Disambiguates whether object has been created in DB
    - Minimizes possibility of creating redundant entries
- Added __setitem__ method to db_object which only allows setting '_id'

Server/load_data_to_mongo.py
@@ -84,7 +84,11 @@ def load_all_items_from_database(repository):
 
 def test_create(repository, new_object):
     print("\n\nSaving new_object to database")
-    repository.create(new_object)
+    result = repository.create(new_object)
+    if result.acknowledged:
+        new_object['_id'] = result.inserted_id
+    else:
+        print("[FAILED] Could not save object")
     print("new_object saved to database")
     print("Loading new_object from database")
     db_objects = repository.read({'_id': new_object._id})

Server/db_object.py
@@ -8,16 +8,16 @@ class DB_Object(object):
 
     def __init__(self, **kwargs):
         if kwargs:
-            self._id = kwargs.get('_id', ObjectId())
             for (k, v) in kwargs.items():
-                if k == '_id':
-                    continue
                 super().__setattr__(k, v)
 
     def get_as_dict(self):
         """ Method returns a dict representing  the DB_Object object,
          this can be written to a JSON file or saved to MongoDB """
         return self.__dict__.copy()
+
+    def get(self, key, def_val):
+        return self.__dict__.get(key, def_val)
 
     @staticmethod
     def build_from_dict(json_data):
@@ -33,6 +33,12 @@ class DB_Object(object):
 
     def __getitem__(self, k):
         return self.__getattribute__(k)
+
+    def __setitem__(self, k, v):
+        if k == '_id':
+            super().__setattr__(k, v)
+        else:
+            raise KeyError("DB_Object only accepts setting of '_id' attribute")
 
     def __str__(self):
         return pformat(self.__dict__)

commit a0c0f7f9ec11808312a140aebb3adbe1efa6a739

Fix bug caused by 8152c276c5896b79f2056799032b4be98f458a05
- Updated build_db function to account for the fact that new ID values are created explicitly by the DB server.

Server/load_data_to_mongo.py
@@ -291,11 +291,20 @@ def build_db(host, port, **kwargs):
 
         # Create DB object from ingredient
         new_ingredient = DB_Object.build_from_dict(ingredient)
-        # Add the new mongoDB id to the existing ingredients dictionary
-        ingredient['_id'] = new_ingredient['_id']
 
         # Insert the ingredient into the database
-        ingredients_db.create(new_ingredient)
+        db_op_res = ingredients_db.create(new_ingredient)
+
+        # Add the new mongoDB id to the existing ingredients dictionary
+        # if the insertion was successful
+        if db_op_res.acknowledged:
+            ingredient['_id'] = db_op_res.inserted_id
+        else:
+            err_msg = (
+                "[FAIL] Database insertion for "
+                + str(new_ingredient)
+                + " was unsuccessful")
+            raise Exception(err_msg)
 
     print("Populating products")
     for product_id in list(products_dict.keys()):